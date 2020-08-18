b016e5f5f47319ed5a92a36cf2e46f6bb23e7113
Jay <jayxxx647@gmail.com>
2020-08-18 18:10:53 +0800

    first

---
+++ assets/js/all.js
@@ -0,0 +1,76 @@
+// Swiper
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  initialSlide : 1,
+  grabCursor : true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      initialSlide : 0,
+    },
+    1024: {
+      slidesPerView: 3,
+      initialSlide : 1,
+    },
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// Loading
+var $loading = $('#loading');
+var $progress = $('#progress');
+window.onload = () => {
+  $loading.hide();
+};
+
+// AOS
+AOS.init({
+  duration: 500,
+});
+
+//Go oop button:
+mybutton = document.getElementById("gotopBtn");
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.opacity = "0";
+  }
+}
+
+// Add shadow of navbar
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scrollVal = $(this).scrollTop();
+
+    if (scrollVal >= 72) {
+      $("#headerNav").addClass("shadow");
+    } else {
+      $("#headerNav").removeClass("shadow");
+    }
+  });
+});
+
+// Works tabs
+$("#works__tabs #works__nav-tabs a").click(function() {
+  var position = $(this).parent().position();
+  var width = $(this).parent().width();
+  $("#works__tabs .slider").css({"left":+ position.left,"width":width});
+});
+var actWidth = $("#works__tabs #works__nav-tabs").find(".active").parent("li").width();
+var actPosition = $("#works__tabs #works__nav-tabs .active").position();
+$("#works__tabs .slider").css({"left":+ actPosition.left,"width": actWidth});
+
